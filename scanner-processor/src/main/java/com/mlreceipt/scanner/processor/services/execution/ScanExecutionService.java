2f672382d22cbc1200721d8a9cbf35838f7d4847
Isaac Guo <isaac_guo@163.com>
2017-08-20 23:35:05 +0800

    can update the scan task entity with scanned text

--- scanner-processor/src/main/java/com/mlreceipt/scanner/processor/services/execution/ScanExecutionService.java
+++ scanner-processor/src/main/java/com/mlreceipt/scanner/processor/services/execution/ScanExecutionService.java
@@ -1,5 +1,7 @@
 package com.mlreceipt.scanner.processor.services.execution;
 
+import com.mlreceipt.scanner.common.entities.ScanTaskEntity;
+
 public interface ScanExecutionService {
-    void execute(String imagePath);
+    void execute(String imagePath, ScanTaskEntity scanTaskEntity);
 }
--- scanner-processor/src/main/java/com/mlreceipt/scanner/processor/services/execution/ScanExecutionServiceImpl.java
+++ scanner-processor/src/main/java/com/mlreceipt/scanner/processor/services/execution/ScanExecutionServiceImpl.java
@@ -1,18 +1,24 @@
 package com.mlreceipt.scanner.processor.services.execution;
 
+import com.mlreceipt.scanner.common.entities.ScanTaskEntity;
+import com.mlreceipt.scanner.common.enums.ScanTaskStatusEnum;
 import com.mlreceipt.scanner.processor.common.ScanExecutorProperties;
+import com.mlreceipt.scanner.processor.feignclients.DataFeignClient;
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecuteResultHandler;
 import org.apache.commons.exec.DefaultExecutor;
 import org.apache.commons.exec.Executor;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -28,20 +34,25 @@ public class ScanExecutionServiceImpl implements ScanExecutionService {
 
     private final TaskExecutor taskExecutor;
     private final Path scannerInputPath;
+    private ScanTaskEntity scanTaskEntity;
+    private final DataFeignClient dataFeignClient;
 
     protected Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    public ScanExecutionServiceImpl(TaskExecutor taskExecutor, ScanExecutorProperties properties) {
+    public ScanExecutionServiceImpl(TaskExecutor taskExecutor, ScanExecutorProperties properties, DataFeignClient dataFeignClient) {
         this.taskExecutor = taskExecutor;
         this.scannerPath = properties.getScannerPath();
         this.scannerOutputPath = Paths.get(properties.getScannerOutputPath());
         this.uploadPath = properties.getUploadPath();
         this.scannerInputPath = Paths.get(properties.getScannerInputPath());
+        this.dataFeignClient=dataFeignClient;
+
     }
 
     @Override
-    public void execute(String imagePath) {
+    public void execute(String imagePath, ScanTaskEntity scanTaskEntity) {
         taskExecutor.execute(new ScanTask(imagePath));
+        this.scanTaskEntity = scanTaskEntity;
     }
 
 
@@ -56,17 +67,42 @@ public class ScanExecutionServiceImpl implements ScanExecutionService {
         public void run() {
 
             //delete files in input/output folder
+            if (!cleanScannerInputAndOutputFolders()) return;
+
+            //execute scanner
+            executeScanner();
+
+            //update scanTaskEntity
+            scanTaskEntity.getScanPairList().stream().forEach(r -> {
+                String textFile=FilenameUtils.removeExtension(r.getImageName())+".txt";
+                Path path=scannerOutputPath.resolve(textFile);
+                r.setText(readFileText(path.toString()));
+            });
+            scanTaskEntity.setStatus(ScanTaskStatusEnum.SCANNED);
+            dataFeignClient.insertScanTask(scanTaskEntity);
+        }
+
+        private String readFileText(String path) {
+            StringBuilder contents = new StringBuilder();
             try {
-                FileUtils.cleanDirectory(scannerInputPath.toFile());
-                FileUtils.cleanDirectory(scannerOutputPath.toFile());
-
-                FileUtils.copyFileToDirectory(fullImagePath.toFile(), scannerInputPath.toFile());
-
-            } catch (IOException e) {
-                e.printStackTrace();
+                BufferedReader input = new BufferedReader(new FileReader(path));
+                try {
+                    String line = null;
+                    while ((line = input.readLine()) != null) {
+                        contents.append(line);
+                        contents.append(System.getProperty("line.separator"));
+                    }
+                } finally {
+                    input.close();
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
+            System.out.println("contents.toString():" + contents.toString());
+            return contents.toString();
+        }
 
-
+        private void executeScanner() {
             if (!StringUtils.isEmpty(scannerPath)) {
                 CommandLine cmdLine = new CommandLine(scannerPath);
                 String[] arguments = {scannerInputPath.toString()};
@@ -75,7 +111,6 @@ public class ScanExecutionServiceImpl implements ScanExecutionService {
 
                 DefaultExecuteResultHandler resultHandler = new DefaultExecuteResultHandler();
 
-                //ExecuteWatchdog watchdog = new ExecuteWatchdog(60 * 1000);
                 Executor executor = new DefaultExecutor();
                 Path workingPath = Paths.get(scannerPath);
 
@@ -88,7 +123,6 @@ public class ScanExecutionServiceImpl implements ScanExecutionService {
                     executor.execute(cmdLine, resultHandler);
                     resultHandler.waitFor();
                     logger.info("Execution finished.");
-
 
                     /*
                     Path p = Paths.get(this.imagePath);
@@ -104,8 +138,29 @@ public class ScanExecutionServiceImpl implements ScanExecutionService {
                     e.printStackTrace();
                 }
             } else {
-                logger.info("ScannerLocation is not specified.");
+                logger.info("Scanner location is not specified.");
             }
+        }
+
+        private boolean cleanScannerInputAndOutputFolders() {
+            try {
+
+                FileUtils.cleanDirectory(scannerInputPath.toFile());
+                FileUtils.cleanDirectory(scannerOutputPath.toFile());
+
+
+                String[] children = this.fullImagePath.toFile().list();
+                for (int i = 0; i < children.length; i++) {
+                    FileUtils.copyFileToDirectory(new File(this.fullImagePath.toFile(), children[i]), scannerInputPath.toFile());
+                }
+
+            } catch (IOException e) {
+                logger.error(e.getMessage());
+                logger.error(e.getStackTrace().toString());
+                e.printStackTrace();
+                return false;
+            }
+            return true;
         }
     }
 }
